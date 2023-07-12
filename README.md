ebcd39243350450506b08c6ba81d6e2df382bf0f
jagroys <jorge.gil1997@hotmail.com>
2023-07-11 22:44:39 -0500

    cambio readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# PruebaJorgeGil
+# Prueba Jorge Andrés Gil R
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.1.4.
 
---
+++ src/app/user-login-component/user-login-component.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserLoginComponentComponent } from './user-login-component.component';
+
+describe('UserLoginComponentComponent', () => {
+  let component: UserLoginComponentComponent;
+  let fixture: ComponentFixture<UserLoginComponentComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [UserLoginComponentComponent]
+    });
+    fixture = TestBed.createComponent(UserLoginComponentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
